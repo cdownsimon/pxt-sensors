2e400eab08c3f8705ef00b26a6b7d2d25a4eeb15
Wong Wai Chung <simon19950213@gmail.com>
2018-06-08 20:10:47 +0800

    update

--- main.ts
+++ main.ts
@@ -87,19 +87,8 @@ namespace sensors {
         const d = pins.pulseIn(echo, PulseValue.High, maxCmDistance * 58);
 
         switch (unit) {
-            case PingUnit.Centimeters: 
-                if (d / 58 < 2){
-                    return 9999;
-                }else{
-                    return d / 58;
-                }
-            case PingUnit.Inches: 
-                if (d / 148 < 0.8){
-                    return 9999;
-                }
-                else{
-                    return d / 148;
-                }
+            case PingUnit.Centimeters: return d / 58;
+            case PingUnit.Inches: return d / 148;
             default: return d;
         }
     }
